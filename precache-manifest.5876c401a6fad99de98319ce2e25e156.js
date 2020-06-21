719f8d8c13c2f0fab9c00be7f214804644dd4738
Yedhu <gyedhumohanan@gmail.com>
2020-06-22 02:55:34 +0530

    Updates

---
+++ precache-manifest.5876c401a6fad99de98319ce2e25e156.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9900d47245f30ccea6e782d2c78d35c9",
+    "url": "/SnakeGame/index.html"
+  },
+  {
+    "revision": "a50016e652e215b22c88",
+    "url": "/SnakeGame/static/css/main.f431b555.chunk.css"
+  },
+  {
+    "revision": "e02bc588089bcd5c253c",
+    "url": "/SnakeGame/static/js/2.c288d001.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SnakeGame/static/js/2.c288d001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a50016e652e215b22c88",
+    "url": "/SnakeGame/static/js/main.9cbcce45.chunk.js"
+  },
+  {
+    "revision": "84e82e47c1273950272b",
+    "url": "/SnakeGame/static/js/runtime-main.c5c5909b.js"
+  }
+]);
